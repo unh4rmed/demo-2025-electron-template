a7aa8ba9a31fbff86d862997e92dd8b82ea9315d
sseezov <sseezov@mail.ru>
2025-02-06 17:34:00 +0700

    template

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,12 +1,10 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
-const api = {}
+const api = {
+  foo: (data) => ipcRenderer.invoke('sendSignal', data)
+}
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,11 +1,17 @@
 import './assets/main.css'
 
+import { StrictMode } from 'react'
+import { Routes, Route, HashRouter } from 'react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HashRouter>
+    <StrictMode>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+      </Routes>
+    </StrictMode>
+  </HashRouter>
 )
